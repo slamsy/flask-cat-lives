90d9b81f660d6c85acb43aa7f3cbfd4465f7ed16
Davey <github@no3.ca>
2018-07-14 13:26:23 -0400

    created first class

--- hello.py
+++ hello.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    x = Hangman()
+    return x.f()
 
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+class Hangman:
+
+    i=12345
+
+    def f(self):
+        return 'boo'
