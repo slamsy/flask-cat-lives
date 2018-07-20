98303c8fcbafcc0201bccc110d89a34eb927d58f
Sharon Lam <sharon@Sharon-Lams-MacBook-Pro.local>
2018-07-20 17:42:34 -0400

    Detab

--- hello.py
+++ hello.py
@@ -19,8 +19,8 @@ class Hangman:
         self.guessedLetters = []
         self.word = self.selectWord()
         self.wordLetters = list(self.word)
-		self.wordLettersRemaining = set(self.wordLetters)
-		self.answer = [''] * len(self.wordLetters)
+        self.wordLettersRemaining = set(self.wordLetters)
+        self.answer = [''] * len(self.wordLetters)
         self.numberOfGuesses = 6
 
     def selectWord(self):
@@ -35,29 +35,29 @@ class Hangman:
         self.checkForWin()
         self.checkForLoss()
 
-	def checkIfGuessed(self,input):
-		for guessedLetter in self.guessedLetters:
-			if input == guessedLetter: 
-				return True #warning message
-			
-	def checkIfCorrect(self,input):	
-		if input in self.wordLetters:
-			self.wordLettersRemaining.remove(input)
-			return True
+    def checkIfGuessed(self,input):
+        for guessedLetter in self.guessedLetters:
+            if input == guessedLetter: 
+                return True #warning message
+            
+    def checkIfCorrect(self,input): 
+        if input in self.wordLetters:
+            self.wordLettersRemaining.remove(input)
+            return True
 
-	def fillInTheBlanks(self,input):
-		index=0
-		while index < len(self.wordLetters):
-			if input == self.wordLetters[index]:
-				self.answer[index] = input
-			index += 1
+    def fillInTheBlanks(self,input):
+        index=0
+        while index < len(self.wordLetters):
+            if input == self.wordLetters[index]:
+                self.answer[index] = input
+            index += 1
 
     def checkForWin(self):
-    	if len(self.wordLettersRemaining) == 0:
-    		return True #Celebratory ASCII
+        if len(self.wordLettersRemaining) == 0:
+            return True #Celebratory ASCII
     
     def checkForLoss(self):
-    	if self.checkIfCorrect() == False:
-        	self.numberofGuesses = self.numberOfGuesses - 1
+        if self.checkIfCorrect() == False:
+            self.numberofGuesses = self.numberOfGuesses - 1
         if self.numberOfGuesses == 0:
-        	return False #Mourning ASCII
+            return False #Mourning ASCII
