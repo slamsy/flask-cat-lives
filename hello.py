0ed123f97fee4733b1aa88e3de6341df09916cb2
Sharon Lam <sharon@Sharon-Lams-MacBook-Pro.local>
2018-07-20 15:39:36 -0400

    Added checking for guessed letters, correct letters, win and loss

--- hello.py
+++ hello.py
@@ -19,6 +19,8 @@ class Hangman:
         self.guessedLetters = []
         self.word = self.selectWord()
         self.wordLetters = list(self.word)
+		self.wordLettersRemaining = set(self.wordLetters)
+		self.Answer = []
         self.numberOfGuesses = 6
 
     def selectWord(self):
@@ -27,8 +29,36 @@ class Hangman:
     def guess(self,letter):
         self.guessedLetters.append(letter)
         self.guessedLetters.sort()
-        self.numberofGuesses = self.numberOfGuesses - 1
+        self.checkIfGuessed()
+        self.checkIfCorrect()
+        self.displayAnswer()
         self.checkForWin()
+        self.checkForLoss()
 
-    def checkForWin(self):
-        return 1
+	def checkIfGuessed(self,input):
+		x = 0
+		while x < len(self.guessedLetters)
+			if input == self.guessedLetters[x]
+				return True #Error
+				else x = x + 1
+		return False
+			
+	def checkIfCorrect(self,input):	
+		if input in self.wordLetters:
+			self.wordLettersRemaining.remove(input)
+			return True
+
+	def displayAnswer():
+		y = self.wordLetters.index(input)
+		if input in self.wordLetters:
+			self.Answer.insert(y,input)	
+
+    def checkForWin():
+    	if len(self.wordLettersRemaining) == 0
+    		return True #Celebratory ASCII
+    
+    def checkForLoss():
+    	if self.checkIfCorrect() == False
+        	self.numberofGuesses = self.numberOfGuesses - 1
+        if self.numberOfGuesses == 0
+        	return False #Mourning ASCII
