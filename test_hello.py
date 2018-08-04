0ed2ade7d879c5cfb3e416532c80833140822159
Davey <github@no3.ca>
2018-08-04 14:47:15 -0400

    bleh one of the great new tests dives into dirt

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,5 @@
 import pytest
+import flask
 
 from hello import app
 
@@ -12,5 +13,10 @@ def client():
 def test_hello(client):
     """basic hello world"""
 
-    rv = client.get('/hello')
-    assert b'hello' in rv.data
+    rv = client.get('/')
+    with client.session_transaction() as sess:
+        sess['hangman'] = '{"CorrectorIncorrect": false, "answer": ["", "", "", ""], "guessedLetters": ["z", "z"], "numberOfGuesses": 6, "numberofGuesses": 5, "word": "test", "wordLetters": ["t", "e", "s", "t"], "wordLettersRemaining": ["t", "e", "s"]}'
+    rv2 = client.get('/?guess=z')
+    blankCount = rv2.data.count(b"blank\'")
+    assert blankCount == 4
+    assert b'5' in rv.data
